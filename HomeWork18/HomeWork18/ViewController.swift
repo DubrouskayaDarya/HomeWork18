fa69467c506619db545d0abf273c98ca1afb39b7
Дарья Дубровская <>
2022-02-16 20:21:33 +0300

    Task 18: Project created

---
+++ HomeWork18/HomeWork18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork18
+//
+//  Created by Дарья Дубровская on 16.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
